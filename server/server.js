96dc7ffb60dad4cb2a84e2e07ee55aae1a0c63bf
josec-lp2 <jose.marinho@academico.ifpb.edu.br>
2023-12-02 21:34:34 -0300

    ttt

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.post('/update-emotion', async (req, res) => {
         }
       });
 
-      await fs.writeFile('data.json', JSON.stringify(jsonData));
+      await fs.writeFile('data.json', JSON.stringify(jsonData, null, 2));
 
       res.status(200).json({ success: true, json: jsonData });
     } else {
